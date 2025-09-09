498ee42e23bd1cac58bb1dfa7971c37979488bd0
Ishlok Dwivedi <ishlokdwivedi1@gmail.com>
2025-09-09 20:10:40 +0530

    Added OkHttp to make Http request

---
+++ src/main/java/main.java
@@ -0,0 +1,25 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        OkHttpClient client=new OkHttpClient();
+
+        String url="https://example.com/";
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response=client.newCall(request).execute()) {
+            if(!response.isSuccessful()){
+                System.out.println("Something wont wrong !!!!!!");
+            }
+            System.out.println(response.body().string());
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
